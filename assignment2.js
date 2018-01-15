ef41da4959544f537f8bc5d242817d9e16e4c500
ejiroghene ifie <22640183+ejiroifie@users.noreply.github.com>
2018-01-15 10:07:41 -0800

    Add files via upload

---
+++ assignment2.js
@@ -0,0 +1,44 @@
+// Exercise #1
+// Write your solution below
+
+var ashkey = '';
+
+for (var i = 0; i <= 6; i++) {
+  ashkey += '#';
+  console.log(ashkey);
+}
+/* 
+Write your explanation for Exercise #1 here
+First we declare the variable
+Next we create the for loop, the for loop declares a variable with a starting value (var i = 0)
+It then evaluates a condition which starts from 0 to 6
+all the code inside the for loop is run line by line, and the value of i increase by 1
+the condition is checked again and if its still true, another number will print out
+The number gets to 6 and it stops
+
+*/
+
+
+
+
+
+// Exercise #2
+// Write your solution below
+
+
+function isEven(num) 
+{
+  if (num%2 == 0)
+  console.log('The number is even');
+else
+ console.log('The number is odd');
+}
+
+
+
+/* 
+Write your explanation for Exercise #2 here
+    declare the function,inside the function we use an if statement for two conditions to be met
+    if there tend to be no remainder then we know num is even else num will be oddd
+   
+*/
